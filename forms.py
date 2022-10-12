28c15f484e9de98b492fe0c778d258cb6160dddc
Diego Forero <dafp18@hotmail.com>
2022-10-12 16:26:35 -0500

    se ajusta incio de sesion, se crea ruta para eliminar los mensajes, se ajustan las vistas y la funcionalidad de las mismas

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField, EmailField   
+from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField, EmailField, TextAreaField   
 from wtforms.validators import DataRequired
+
 
 class LoginForm (FlaskForm):
     email = EmailField('Correo electrónico', validators=[DataRequired(message = 'Debe ingresar un correo electrónico válido')])
@@ -15,3 +16,10 @@ class RegisterForm (FlaskForm):
     password =PasswordField('Contraseña')
     confirmPassword = PasswordField('Confirmar contraseña')
     submit = SubmitField('Registrarse')
+
+
+class NewMessageForm (FlaskForm):
+    to = EmailField('Para', validators=[DataRequired(message = 'Debe ingresar un correo electrónico válido')])
+    subject = StringField('Asunto', validators=[DataRequired(message = 'Debe ingresar un asunto')])
+    message =TextAreaField('Mensaje')
+    submitBtn = SubmitField('Enviar mensaje')    
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask,request,render_template,flash
+import functools
+from flask import Flask,request,render_template,flash, redirect, url_for, g, make_response, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3 as sql
 import os
 import forms
 import utils
+
 
 SECRET_KEY = os.urandom(32)
 
@@ -17,12 +20,32 @@ def index():
     route = '/ Inicio'
     formRegister = forms.RegisterForm()
     formLogin = forms.LoginForm()
-    
+
     if(formLogin.validate_on_submit() and request.method == 'POST'):
         email = formLogin.email.data
         password = formLogin.password.data
-        remember = formLogin.remember.data
-        flash('usuario {}, password {}, recordar {}'.format( email, password, remember ))
+        con=sql.connect('database/MailTicDatabase.db')
+        cur=con.cursor()
+        user = cur.execute('select * from users where email = ? and password = ?', (email,password)).fetchone()
+        
+        if user is None:
+            user = cur.execute('select * from users where email = ?', (email,)).fetchone()
+            if user is None:
+                flash('El correo electrónico no existe')
+            else:
+                pass_almacenado = user[3]
+                resultado = check_password_hash(pass_almacenado,password)
+                if resultado is False:
+                    flash('La contraseña es incorrecta')
+                else:
+                    session.clear()
+                    session['user_id'] = user[0]
+                    res = make_response(redirect(url_for('welcome')))
+                    return res
+        else:
+            session.clear()
+            session['user_id'] = user[0]
+            return redirect(url_for('welcome'))
         
     return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert )    
     
@@ -31,26 +54,55 @@ def index():
 def welcome():
     title='Bienvenida'
     route = '/ Bienvenida'
-    return render_template('bienvenida.html', title=title, route=route)
+    if 'user_id' in session:
+        return render_template('bienvenida.html', title=title, route=route)
+    else:
+        return 'unAuthorized'
 
-@app.route('/newMessage')
+@app.route('/newMessage', methods=['GET','POST'])
 def newMessage():
     title = 'Mensaje nuevo'
     route = '/ Mensaje nuevo'
-    return render_template('newMessage.html', title=title, route=route)
-    #params = request.args.get('nombre_param', 'valor_por_defecto')   
-    #return 'El parametro es {}'.format(params) 
+    formMessage = forms.NewMessageForm()
+    if 'user_id' in session:
+        return render_template('newMessage.html', title=title, route=route, formMessage=formMessage)
+    else:
+        return 'unAuthorized'
+
+@app.route('/createMessage', methods=['GET','POST'])
+def createMessage():
+    formMessage = forms.NewMessageForm()
+    if(formMessage.validate_on_submit()):
+        to = formMessage.to.data
+        subject = formMessage.subject.data
+        message = formMessage.message.data
+        con=sql.connect('database/MailTicDatabase.db')
+        cur=con.cursor()
+        user_to_id = cur.execute('select id from users where email = ?', (to,)).fetchone()
+        if user_to_id is not None:
+            user_to_id = user_to_id[0]
+            cur.execute('INSERT INTO messages ( subject,message,id_user_to,id_user_from) values (?,?,?,?)', (subject,message,user_to_id,4))
+            con.commit()
+        else:
+            flash('El correo electrónico de destino no existe') 
+        
+    return redirect(url_for('newMessage'))
+
 
 @app.route('/messages')
 def messages():
     title = 'Bandeja de entrada'
     route = '/ Bandeja de entrada'
-    con=sql.connect('MailTicDatabase')
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur=execute('select * from users')
-    data=cur.fetchall()
-    return render_template('messages.html', title=title, route=route, data=data)
+    
+    if 'user_id' in session:
+        userSession = session['user_id']
+        con=sql.connect('database/MailTicDatabase.db')
+        cur=con.cursor()
+        messages = cur.execute('select a.id as idmsg, a.subject, a.message, a.created_at, b.id as iduser, b.name, b.email from messages a inner join users b on b.id = id_user_to where id_user_to = ?', (userSession,)).fetchall()
+        print(messages)
+        return render_template('messages.html', title=title, route=route, messages=messages)
+    else:
+        return 'unAuthorized'
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
@@ -66,11 +118,6 @@ def add_user():
             password = formRegister.password.data
             confirmPassword = formRegister.confirmPassword.data
             error = None
-        
-            if not utils.isUsernameValid(name):
-                error = "el usuario debe ser alfanumerico o incluir solo ' . ',  ' _ ', ' - ' "
-                flash(error)
-                
             
             if not utils.isEmailValid(email):
                 error = 'Debe ingresar un correo electrónico válido'
@@ -87,8 +134,8 @@ def add_user():
                 colorAlert = "alert-success"
                 try:
                     with sql.connect("database/MailTicDatabase.db") as con:
-                        cur = con.cursor()
-                        cur.execute( "INSERT INTO users ( name,email,password,id_rol ) VALUES ( ?,?,?,? )", (name,email,password,1) )
+                        cur = con.cursor()                       
+                        cur.execute( "INSERT INTO users ( name,email,password,id_rol ) VALUES ( ?,?,?,? )", (name,email,generate_password_hash(password),1) )
                         con.commit()
                         flash('Cuenta creada correctamente revisa tu correo para activarla.')
                 except:
@@ -100,17 +147,31 @@ def add_user():
             return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert )
     return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert ) 
 
-@app.route('/update_user/<string:id>', methods=['PUT'])
-def edit_user():
-    return 'update_user'
+@app.route('/delete_msg/<idmsg>')
+def delete_msg(idmsg):
+    if 'user_id' in session:
+        con=sql.connect('database/MailTicDatabase.db')
+        cur=con.cursor()
+        cur.execute('delete from messages where id = ?', (idmsg,))
+        con.commit()
+        return redirect(url_for('messages'))
+    else:
+        return 'unAuthorized'
 
-@app.route('/edit_user/<string:id>', methods=['GET', 'POST'])
-def edit_user():
-    return 'edit_user'
+@app.route('/logout')
+def logout():
+    session.clear()
+    return 'login'
 
-@app.route('/delete_user', methods=['DELETE'])
-def delete_user():
-    return 'delete_user'        
+
+def login_reqeuired(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('/'))
+        
+        return view(**kwargs)
+    return wrapped_view
 
 
 if __name__ == '__main__':    
