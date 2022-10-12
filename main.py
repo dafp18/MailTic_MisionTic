387b019263e288c6b591f6ec266b658cb37d0642
Diego Forero <dafp18@hotmail.com>
2022-10-12 09:41:10 -0500

    se crea conexion con la base de datos y se prueba insercion de usuario nuevo, adicional se agrega alerts para ver el proceso realizado

--- main.py
+++ main.py
@@ -1,55 +1,29 @@
 from flask import Flask,request,render_template,flash
+import sqlite3 as sql
 import os
-SECRET_KEY = os.urandom(32)
 import forms
 import utils
 
+SECRET_KEY = os.urandom(32)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+
 
 @app.route('/', methods=['GET','POST'])
 def index():
     colorAlert = "alert-danger"
     title='Inicio'
     route = '/ Inicio'
-    formLogin = forms.LoginForm()
     formRegister = forms.RegisterForm()
+    formLogin = forms.LoginForm()
+    
     if(formLogin.validate_on_submit() and request.method == 'POST'):
         email = formLogin.email.data
         password = formLogin.password.data
         remember = formLogin.remember.data
         flash('usuario {}, password {}, recordar {}'.format( email, password, remember ))
-        pass
-    if(formRegister.validate_on_submit() and request.method == 'POST'):
-        try:
-            name = formRegister.name.data
-            email = formRegister.email.data
-            password = formRegister.password.data
-            confirmPassword = formRegister.confirmPassword.data
-            error = None
         
-            if not utils.isUsernameValid(name):
-                error = "el usuario debe ser alfanumerico o incluir solo '.',  '_', '-' "
-                flash(error)
-                
-            
-            if not utils.isEmailValid(email):
-                error = 'Debe ingresar un correo electrónico válido'
-                flash(error)
-
-            if not utils.isPasswordValid(password):
-                error = 'La contraseña debe contener al menos 8 caracteres una letra mayúscula, un número y un símbolo'
-                flash(error)
-            else:
-                if(password != confirmPassword):
-                    error = 'Las contraseñas no coinciden'    
-                    flash(error)
-            if not error:
-                colorAlert = "alert-success"
-                flash('Cuenta creada correctamente revisa tu correo para activarla.')
-                return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert )
-        except:
-            return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert )
     return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert )    
     
 
@@ -71,7 +45,72 @@ def newMessage():
 def messages():
     title = 'Bandeja de entrada'
     route = '/ Bandeja de entrada'
-    return render_template('messages.html', title=title, route=route)
+    con=sql.connect('MailTicDatabase')
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur=execute('select * from users')
+    data=cur.fetchall()
+    return render_template('messages.html', title=title, route=route, data=data)
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    colorAlert = "alert-danger"
+    title='Inicio'
+    route = '/ Inicio'
+    formLogin = forms.LoginForm()
+    formRegister = forms.RegisterForm()
+    if(formRegister.validate_on_submit() and request.method == 'POST'):
+        try:
+            name = formRegister.name.data
+            email = formRegister.email.data
+            password = formRegister.password.data
+            confirmPassword = formRegister.confirmPassword.data
+            error = None
+        
+            if not utils.isUsernameValid(name):
+                error = "el usuario debe ser alfanumerico o incluir solo ' . ',  ' _ ', ' - ' "
+                flash(error)
+                
+            
+            if not utils.isEmailValid(email):
+                error = 'Debe ingresar un correo electrónico válido'
+                flash(error)
+
+            if not utils.isPasswordValid(password):
+                error = 'La contraseña debe contener al menos 8 caracteres una letra mayúscula, un número y un símbolo'
+                flash(error)
+            else:
+                if(password != confirmPassword):
+                    error = 'Las contraseñas no coinciden'    
+                    flash(error)
+            if not error:
+                colorAlert = "alert-success"
+                try:
+                    with sql.connect("database/MailTicDatabase.db") as con:
+                        cur = con.cursor()
+                        cur.execute( "INSERT INTO users ( name,email,password,id_rol ) VALUES ( ?,?,?,? )", (name,email,password,1) )
+                        con.commit()
+                        flash('Cuenta creada correctamente revisa tu correo para activarla.')
+                except:
+                    con.rollback()
+                    flash('Upss ha ocurrido un error intente más tarde.')
+                                    
+                return render_template('inicio.html', title=title, route=route, formRegister=formRegister,colorAlert=colorAlert )
+        except:
+            return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert )
+    return render_template('inicio.html', title=title, route=route, formLogin=formLogin,formRegister=formRegister,colorAlert=colorAlert ) 
+
+@app.route('/update_user/<string:id>', methods=['PUT'])
+def edit_user():
+    return 'update_user'
+
+@app.route('/edit_user/<string:id>', methods=['GET', 'POST'])
+def edit_user():
+    return 'edit_user'
+
+@app.route('/delete_user', methods=['DELETE'])
+def delete_user():
+    return 'delete_user'        
 
 
 if __name__ == '__main__':    
